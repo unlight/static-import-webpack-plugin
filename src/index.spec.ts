dea0faf8eb75c18e4b3e8cebb13c1653088751f8
Roman <s91630277@gmail.com>
2019-08-21 02:12:31 +0400

    feat: First implementation

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,17 +1,26 @@
-/* eslint-disable @typescript-eslint/tslint/config */
 import * as lib from './index';
 import * as webpack from 'webpack';
+import { esmImportPlugin } from '.';
 import MemoryFS = require('memory-fs');
-import SingleEntryDependency = require('webpack/lib/dependencies/SingleEntryDependency');
 
 const fs = new MemoryFS();
-fs.mkdirpSync('/src');
 
 const compiler = webpack({
     context: '/src',
     entry: './entry.js',
     mode: 'development',
     devtool: false,
+    module: {
+        rules: [
+            {
+                parser: {
+                    amd: false,
+                    system: false,
+                    harmony: true,
+                }
+            },
+        ],
+    },
     output: {
         filename: 'output.js',
         path: '/dist',
@@ -26,6 +35,7 @@ compiler.outputFileSystem = fs;
 (compiler as any).resolvers.context.fileSystem = fs;
 
 function compile() {
+    esmImportPlugin(compiler);
     return new Promise<webpack.Stats>((resolve, reject) => {
         compiler.run((err, stats) => {
             if (err) {
@@ -36,11 +46,44 @@ function compile() {
     });
 }
 
-it('smoke test', async () => {
-    fs.writeFileSync('/src/entry.js', 'console.log(`Hi there`)');
-    // fs.writeFileSync('/src/entry.js', 'require("./dependency.js")');
-    // fs.writeFileSync('/src/dependency.js', 'module.exports = "this is dependency"');
+function removeWebpackProlog(source: string) {
+    const marks = ['/******/', '/************************************************************************/'];
+    const startIndex = source.indexOf(marks[0]);
+    const endIndex = source.lastIndexOf(marks[1]);
+    const result = source.slice(0, startIndex) + source.slice(endIndex + marks[1].length);
+    return result.trim();
+}
+
+beforeEach(() => {
+    fs.mkdirpSync('/src');
+});
+
+afterEach(() => {
+    fs.rmdirSync('/src');
+});
+
+it('smoke', async () => {
+    fs.writeFileSync('/src/entry.js', `console.log('Hi there')`);
     const stats = await compile();
-    const appSource = stats.compilation.assets['output.js'].source();
-    expect(appSource).toContain('console.log(`Hi there`)');
+    const output = stats.compilation.assets['output.js'].source();
+    expect(output).toContain(`console.log('Hi there')`);
+});
+
+it('import smoke', async () => {
+    fs.writeFileSync('/src/foo.js', `export const foo = () => console.log('foo')`);
+    fs.writeFileSync('/src/entry.js', `import { foo } from './foo'`);
+    const stats = await compile();
+    const output = removeWebpackProlog(stats.compilation.assets['output.js'].source());
+    expect(output).toContain(`/* harmony import */ var _foo__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./foo */ "./foo.js")`);
+});
+
+it('static import with options', async () => {
+    // fs.writeFileSync('/src/foo.js', `export const foo = () => console.log('foo')`);
+    // fs.writeFileSync('/src/bar.js', `console.log('bar')`);
+    fs.writeFileSync('/src/entry.js', `import /* webpackIgnore: true */ './foo';`);
+    const stats = await compile();
+    const output = removeWebpackProlog(stats.compilation.assets['output.js'].source())
+        .split('\n')
+        .find((value, index) => index === 0);
+    expect(output).toContain(`import './foo'`);
 });
