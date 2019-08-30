082f5fade0cc6c0a65c70a1bcb2641abdb596f68
Roman <s91630277@gmail.com>
2019-08-31 01:30:58 +0400

    fix: Removed duplicates

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,101 +5,107 @@ import MemoryFS = require('memory-fs');
 
 const fs = new MemoryFS();
 
-const compiler = webpack({
-    context: '/src',
-    entry: './entry.js',
-    mode: 'development',
-    devtool: false,
-    module: {
-        rules: [
-            {
-                parser: {
-                    amd: false,
-                    system: false,
-                    harmony: true,
-                }
-            },
+async function compile(customOptions = {}) {
+    const options = Object.assign({
+        context: '/',
+        entry: '/entry',
+        mode: 'development',
+        devtool: false,
+        resolve: {
+            extensions: ['.js', '.ts', '.tsx', '.json'],
+        },
+        module: {
+            rules: [
+                {
+                    parser: {
+                        amd: false,
+                        system: false,
+                        harmony: true,
+                    }
+                },
+                {
+                    test: /\.js$/,
+                    type: 'javascript/esm',
+                },
+            ],
+        },
+        output: {
+            filename: 'output.js',
+            path: '/dist',
+            libraryTarget: 'var',
+            library: '$lib',
+        },
+        plugins: [
         ],
-    },
-    output: {
-        filename: 'output.js',
-        path: '/dist',
-        libraryTarget: 'var',
-        library: '$lib',
-    },
-});
+    }, customOptions);
+    const compiler = webpack(<any>options);
 
-compiler.inputFileSystem = fs;
-compiler.outputFileSystem = fs;
-(<any>compiler).resolvers.normal.fileSystem = fs;
-(<any>compiler).resolvers.context.fileSystem = fs;
+    compiler.inputFileSystem = fs;
+    compiler.outputFileSystem = fs;
+    (<any>compiler).resolvers.normal.fileSystem = fs;
+    (<any>compiler).resolvers.context.fileSystem = fs;
 
-async function compile() {
     staticImportWebpackPlugin(compiler);
     return new Promise<webpack.Stats>((resolve, reject) => {
         compiler.run((error, stats) => {
             if (error) {
                 return reject(error);
             }
+            let { errors, warnings } = stats.toJson({ all: true });
+            errors = [...errors, ...warnings];
+            if (errors.length > 0) {
+                return reject(errors);
+            }
             resolve(stats);
         });
     });
 }
 
-function removeWebpackProlog(source: string) {
+function getOutput(source: string, { lines = 0 } = {}) {
     const marks = ['/******/', '/************************************************************************/'];
     const startIndex = source.indexOf(marks[0]);
     const endIndex = source.lastIndexOf(marks[1]);
-    const result = source.slice(0, startIndex) + source.slice(endIndex + marks[1].length);
+    let result = source.slice(0, startIndex) + source.slice(endIndex + marks[1].length);
+    if (lines > 0) {
+        result = result.split('\n').slice(0, lines).join('\n');
+    }
     return result.trim();
 }
 
-beforeEach(() => {
-    fs.mkdirpSync('/src');
-});
-
-afterEach(() => {
-    fs.rmdirSync('/src');
-});
-
 it('smoke', async () => {
-    fs.writeFileSync('/src/entry.js', `console.log('Hi there')`);
+    fs.writeFileSync('/foo.js', `export const foo = () => console.log('foo')`);
+    fs.writeFileSync('/entry.js', `
+        import { foo } from './foo'
+        console.log('Hi there')
+    `);
     const stats = await compile();
     const output = stats.compilation.assets['output.js'].source();
     expect(output).toContain(`console.log('Hi there')`);
 });
 
-it('import smoke', async () => {
-    fs.writeFileSync('/src/foo.js', `export const foo = () => console.log('foo')`);
-    fs.writeFileSync('/src/entry.js', `import { foo } from './foo'`);
-    const stats = await compile();
-    const output = removeWebpackProlog(stats.compilation.assets['output.js'].source());
-    expect(output).toContain(`/* harmony import */ var _foo__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./foo */ "./foo.js")`);
-});
-
 it('static import with options', async () => {
-    fs.writeFileSync('/src/entry.js', `import /* webpackIgnore: true */ './foo'`);
+    fs.writeFileSync('/entry.js', `import /* webpackIgnore: true */ './foo'`);
     const stats = await compile();
-    const output = removeWebpackProlog(stats.compilation.assets['output.js'].source());
+    const output = getOutput(stats.compilation.assets['output.js'].source());
     expect(output).toContain(`import './foo'`);
     expect(output).not.toContain(`import /* webpackIgnore: true */ './foo'`);
 });
 
 it('static import all', async () => {
-    fs.writeFileSync('/src/entry.js', `import * as all /* webpackIgnore: true */ from './all'`);
+    fs.writeFileSync('/entry.js', `import * as all /* webpackIgnore: true */ from './all'`);
     const stats = await compile();
-    const output = removeWebpackProlog(stats.compilation.assets['output.js'].source());
+    const output = getOutput(stats.compilation.assets['output.js'].source());
     expect(output).toContain(`import * as all from './all'`);
     expect(output).not.toContain(`import * as all /* webpackIgnore: true */ from './all'`);
 });
 
 it('multiple static import', async () => {
-    fs.writeFileSync('/src/entry.js', `
+    fs.writeFileSync('/entry.js', `
         import pokemon /* webpackIgnore: true */ from './pokemon';
         import /* webpackIgnore: true */ * as bundledUnicorn from './unicorn';
         `);
     const stats = await compile();
-    const output = removeWebpackProlog(stats.compilation.assets['output.js'].source());
+    const output = getOutput(stats.compilation.assets['output.js'].source());
     expect(output).toContain(`import pokemon from './pokemon'`);
     expect(output).toContain(`import * as bundledUnicorn from './unicorn'`);
     expect(output).not.toContain(`import pokemon /* webpackIgnore: true */ from './pokemon'`);
@@ -107,14 +113,37 @@ it('multiple static import', async () => {
 });
 
 it('referencing ignored static import in bundle', async () => {
-    fs.writeFileSync('/src/entry.js', `
+    fs.writeFileSync('/entry.js', `
         import pokemon /* webpackIgnore: true */ from 'http://example.com/pokemon';
         pokemon('hi');
         console.log(typeof pokemon);
         export { pokemon };
         `);
     const stats = await compile();
-    const output = removeWebpackProlog(stats.compilation.assets['output.js'].source());
+    const output = getOutput(stats.compilation.assets['output.js'].source());
     expect(output).toContain(`import pokemon from 'http://example.com/pokemon'`);
     expect(output).not.toContain(`MODULE_NOT_FOUND`);
+});
+
+it('multiple import duplicates', async () => {
+    fs.writeFileSync('/entry.js', `
+        import A /* webpackIgnore: true */ from 'a';
+        import /* webpackIgnore: true */ 'b';
+        `);
+    const stats = await compile();
+    const output = getOutput(stats.compilation.assets['output.js'].source(), { lines: 3 });
+    expect(output).not.toContain(`import A from 'a';\nimport A from 'a';`);
+});
+
+it('import side effects', async () => {
+    fs.writeFileSync('/entry.js', `
+        import /* webpackIgnore: true */ 'a';
+        import /* webpackIgnore: true */ 'b';
+        `);
+    const stats = await compile();
+    const output = getOutput(stats.compilation.assets['output.js'].source(), { lines: 3 });
+    expect(output).toContain(`import 'a'`);
+    expect(output).toContain(`import 'b'`);
+    expect(output).not.toContain(`import /* webpackIgnore: true */ 'a'`);
+    expect(output).not.toContain(`import /* webpackIgnore: true */ 'b'`);
 });
--- src/index.ts
+++ src/index.ts
@@ -12,43 +12,30 @@ export class StaticImportWebpackPlugin {
 }
 
 export function staticImportWebpackPlugin(compiler: webpack.Compiler) {
-
     const importSources = new WeakMap();
-
-    compiler.hooks.thisCompilation.tap('StaticImport', (compilation, { normalModuleFactory }) => {
-
-        normalModuleFactory.hooks.parser
-            .for('javascript/auto')
-            .tap('StaticImport', (parser, parserOptions) => {
-                parser.hooks.import.tap('StaticImport', importHandler.bind(undefined, importSources, parser));
-            });
-
-        compilation.hooks.succeedModule.tap('StaticImport', (module: any) => {
-            if (importSources.has(module)) {
-                module.dependencies = module.dependencies.filter(filterDependency);
-            }
-        });
-
-        compilation.hooks.optimizeChunkAssets.tap('StaticImport', (chunks) => {
-            chunks.forEach(chunk => {
-                if (importSources.has(chunk.entryModule)) {
-                    chunk.files.forEach(fileName => {
-                        compilation.assets[fileName] = new ConcatSource(
-                            importsForModule(importSources, chunk.entryModule),
-                            '\n\n',
-                            compilation.assets[fileName],
-                        );
-                    });
-                }
-            });
-        });
-    });
-
+    compiler.hooks.thisCompilation.tap('StaticImport', thisCompilationTap.bind(undefined, importSources));
 }
 
-function importHandler(importSources, parser, statement, source) {
-    const { options, errors } = parser.parseCommentOptions(statement.range);
+function thisCompilationTap(importSources, compilation, { normalModuleFactory }) {
+    const boundParserImportTap = parserImportTap.bind(undefined, importSources);
+    normalModuleFactory.hooks.parser.for('javascript/auto').tap('StaticImport', boundParserImportTap);
+    normalModuleFactory.hooks.parser.for('javascript/dynamic').tap('StaticImport', boundParserImportTap);
+    normalModuleFactory.hooks.parser.for('javascript/esm').tap('StaticImport', boundParserImportTap);
+    compilation.hooks.optimizeChunkAssets.tap('StaticImport', compilationOptimizeChunkAssetsTap.bind(undefined, compilation, importSources));
+}
 
+function parserImportTap(importSources, parser, parserOptions) {
+    const boundImportTap = importTap.bind(undefined, importSources, parser);
+    parser.hooks.import.tap('StaticImport', boundImportTap);
+    parser.hooks.importSpecifier.tap('StaticImport', boundImportTap);
+}
+
+function importTap(importSources, parser, statement, source, specifier, name) {
+    const module = parser.state.module;
+    if (importSources.has(module) && importSources.get(module).includes(statement)) {
+        return false;
+    }
+    const { options, errors } = parser.parseCommentOptions(statement.range);
     if (errors) {
         const warnings = errors.map(error => {
             return new CommentCompilationWarning(
@@ -57,41 +44,53 @@ function importHandler(importSources, parser, statement, source) {
                 error.comment.loc,
             );
         });
-        parser.state.module.warnings.push(warnings);
+        if (warnings.length > 0) {
+            parser.state.module.warnings.push(...warnings);
+        }
     }
-
     if (options && options.webpackIgnore) {
+        (generate);
+        debugger
         const module = parser.state.module;
         if (!importSources.has(module)) {
             importSources.set(module, []);
         }
         importSources.get(module).push(statement);
+        module.dependencies
+            .filter(isHarmonyImportDependency)
+            .forEach(dependency => module.removeDependency(dependency));
 
-        const clearDependency = new ConstDependency('', statement.range);
-        clearDependency.loc = statement.loc;
-        module.addDependency(clearDependency);
-
+        module.addDependency(new ConstDependency('', statement.range));
         return false;
     }
 }
 
-function filterDependency(dependency: { constructor: { name: string } }) {
-    return ![
-        'HarmonyExportDependencyTemplate',
-        'HarmonyCompatibilityDependency',
-        'HarmonyInitDependency',
-        'HarmonyInitDependencyTemplate',
-        'HarmonyImportSpecifierDependency',
-        'HarmonyImportSpecifierDependencyTemplate',
-        'HarmonyImportSideEffectDependency',
-        'HarmonyImportSideEffectDependencyTemplate',
-        // 'HarmonyAcceptImportDependency',
-        // 'HarmonyAcceptImportDependencyTemplate',
-    ].includes(dependency.constructor.name);
+function compilationOptimizeChunkAssetsTap(compilation, importSources, chunks) {
+    chunks.forEach(chunk => {
+        debugger;
+        if (importSources.has(chunk.entryModule)) {
+            chunk.files.forEach(fileName => {
+                compilation.assets[fileName] = new ConcatSource(
+                    importsForModule(importSources, chunk.entryModule),
+                    '\n\n',
+                    compilation.assets[fileName],
+                );
+            });
+        }
+    });
 }
 
 function importsForModule(importSources: WeakMap<object, any>, module: any) {
     return importSources.get(module)
         .map(generate)
         .join('\n');
+}
+
+function isHarmonyImportDependency(dependency: { constructor: { name: string } }) {
+    return [
+        'HarmonyCompatibilityDependency',
+        'HarmonyInitDependency',
+        'HarmonyImportSpecifierDependency',
+        // 'HarmonyImportSideEffectDependency',
+    ].includes(dependency.constructor.name);
 }
